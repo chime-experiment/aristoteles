bc3f20cecc802eab8d73d53790e3dbf64f7875bd
Rik van Lieshout <rik.lieshout@gmail.com>
2026-08-14 16:43:48 +0200

    build: switch build to pyproject.toml (#7)
    And use setuptools-git-versioning in stead of setuptools_scm

--- aristoteles/__init__.py
+++ aristoteles/__init__.py
@@ -2,12 +2,12 @@
 # To find the version number for an uninstalled copy of
 # this package, execute:
 #
-#   python setup.py --version
+#   setuptools-git-versioning
 #
-from pkg_resources import get_distribution, DistributionNotFound
+from importlib.metadata import version, PackageNotFoundError
 
 try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
+    __version__ = version(__name__)
+except PackageNotFoundError:
     # package is not installed
     pass
